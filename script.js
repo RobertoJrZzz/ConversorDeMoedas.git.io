d5beffa6885a6243e682a366a493aba9174a4ad4
Roberto Junior <166872356+RobertoJrZzz@users.noreply.github.com>
2024-08-27 14:20:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,431 @@
+function Conversor(){
+    let n1 = parseInt(document.querySelector("#value").value)
+    let operation01 = document.querySelector(".coin-01").value
+    let operation02 = document.querySelector(".coin-02").value
+    let resultado = document.querySelector("#result")
+
+    switch(operation01){
+        case "Dolar Americano":
+           switch(operation02){
+            case "Euro":
+            
+        Euro = 0.92
+        Real = 5.10
+        Iene = 155.89
+        Peso = 883.24
+        
+        calculo = n1 * Euro
+        resultado.innerHTML = calculo.toFixed(2)
+        break
+    }
+    break
+    }
+
+    switch(operation01){
+        case "Dolar Americano":
+           switch(operation02){
+            case "Real":
+            
+        Euro = 0.92
+        Real = 5.10
+        Iene = 155.89
+        Peso = 883.24
+        
+        calculo = n1 * Real
+        resultado.innerHTML = calculo.toFixed(2)
+        break
+    }
+    break
+    }
+
+    switch(operation01){
+        case "Dolar Americano":
+           switch(operation02){
+            case "Iene":
+            
+        Euro = 0.92
+        Real = 5.10
+        Iene = 155.89
+        Peso = 883.24
+        
+        calculo = n1 * Iene
+        resultado.innerHTML = calculo.toFixed(2)
+        break
+    }
+    break
+    }
+    
+    switch(operation01){
+        case "Dolar Americano":
+           switch(operation02){
+            case "Peso":
+            
+        Euro = 0.92
+        Real = 5.10
+        Iene = 155.89
+        Peso = 883.24
+        
+        calculo = n1 * Peso
+        resultado.innerHTML = calculo.toFixed(2)
+        break
+    }
+    break
+    }
+    
+    switch(operation01){
+        case "Dolar Americano":
+           switch(operation02){
+            case "Dolar Americano":
+        Proprio = 1
+        Euro = 0.92
+        Real = 5.10
+        Iene = 155.89
+        Peso = 883.24
+        
+        calculo = n1 * Proprio
+        resultado.innerHTML = calculo.toFixed(2)
+        break
+    }
+    break
+    }
+
+    switch(operation01){
+        case "Euro":
+           switch(operation02){
+            case "Euro":
+        Proprio = 1
+        DolarAmericano = 1.09
+        Real = 5.55
+        Iene = 169.75
+        Peso = 965.42
+        
+        calculo = n1 * Proprio
+        resultado.innerHTML = calculo.toFixed(2)
+        break
+    }
+    break
+    }
+    
+    switch(operation01){
+        case "Euro":
+           switch(operation02){
+            case "Dolar Americano":
+        Proprio = 1
+        DolarAmericano = 1.09
+        Real = 5.55
+        Iene = 169.75
+        Peso = 965.42
+        
+        calculo = n1 * DolarAmericano
+        resultado.innerHTML = calculo.toFixed(2)
+        break
+    }
+    break
+    }
+    
+    switch(operation01){
+        case "Euro":
+           switch(operation02){
+            case "Real":
+        Proprio = 1
+        DolarAmericano = 1.09
+        Real = 5.55
+        Iene = 169.75
+        Peso = 965.42
+        
+        calculo = n1 * Real
+        resultado.innerHTML = calculo.toFixed(2)
+        break
+    }
+    break
+    }
+    
+    switch(operation01){
+        case "Euro":
+           switch(operation02){
+            case "Iene":
+        Proprio = 1
+        DolarAmericano = 1.09
+        Real = 5.55
+        Iene = 169.75
+        Peso = 965.42
+        
+        calculo = n1 * Iene
+        resultado.innerHTML = calculo.toFixed(2)
+        break
+    }
+    break
+    }
+    
+    switch(operation01){
+        case "Euro":
+           switch(operation02){
+            case "Peso":
+        Proprio = 1
+        DolarAmericano = 1.09
+        Real = 5.55
+        Iene = 169.75
+        Peso = 965.42
+        
+        calculo = n1 * Peso
+        resultado.innerHTML = calculo.toFixed(2)
+        break
+    }
+    break
+    }
+   
+    switch(operation01){
+        case "Real":
+           switch(operation02){
+            case "Real":
+        Proprio = 1
+        DolarAmericano = 0.20
+        Euro = 0.18
+        Iene = 30.59
+        Peso = 173.99
+        
+        calculo = n1 * Proprio
+        resultado.innerHTML = calculo.toFixed(2)
+        break
+    }
+    break
+    }
+    
+    switch(operation01){
+        case "Real":
+           switch(operation02){
+            case "Dolar Americano":
+        Proprio = 1
+        DolarAmericano = 0.20
+        Euro = 0.18
+        Iene = 30.59
+        Peso = 173.99
+        
+        calculo = n1 * DolarAmericano
+        resultado.innerHTML = calculo.toFixed(2)
+        break
+    }
+    break
+    }
+
+    switch(operation01){
+        case "Real":
+           switch(operation02){
+            case "Euro":
+        Proprio = 1
+        DolarAmericano = 0.20
+        Euro = 0.18
+        Iene = 30.59
+        Peso = 173.99
+        
+        calculo = n1 * Euro
+        resultado.innerHTML = calculo.toFixed(2)
+        break
+    }
+    break
+    }
+
+    switch(operation01){
+        case "Real":
+           switch(operation02){
+            case "Iene":
+        Proprio = 1
+        DolarAmericano = 0.20
+        Euro = 0.18
+        Iene = 30.59
+        Peso = 173.99
+        
+        calculo = n1 * Iene
+        resultado.innerHTML = calculo.toFixed(2)
+        break
+    }
+    break
+    }
+
+    switch(operation01){
+        case "Real":
+           switch(operation02){
+            case "Peso":
+        Proprio = 1
+        DolarAmericano = 0.20
+        Euro = 0.18
+        Iene = 30.59
+        Peso = 173.99
+        
+        calculo = n1 * Peso
+        resultado.innerHTML = calculo.toFixed(2)
+        break
+    }
+    break
+    }
+
+    switch(operation01){
+        case "Iene":
+           switch(operation02){
+            case "Iene":
+        Proprio = 1
+        DolarAmericano = 0.0064
+        Euro = 0.0059
+        Real = 0.033
+        Peso = 5.69
+        
+        calculo = n1 * Proprio
+        resultado.innerHTML = calculo.toFixed(2)
+        break
+    }
+    break
+    }
+
+    switch(operation01){
+        case "Iene":
+           switch(operation02){
+            case "Dolar Americano":
+        Proprio = 1
+        DolarAmericano = 0.0064
+        Euro = 0.0059
+        Real = 0.033
+        Peso = 5.69
+        
+        calculo = n1 * DolarAmericano
+        resultado.innerHTML = calculo.toFixed(2)
+        break
+    }
+    break
+    }
+
+    switch(operation01){
+        case "Iene":
+           switch(operation02){
+            case "Euro":
+        Proprio = 1
+        DolarAmericano = 0.0064
+        Euro = 0.0059
+        Real = 0.033
+        Peso = 5.69
+        
+        calculo = n1 * Euro
+        resultado.innerHTML = calculo.toFixed(2)
+        break
+    }
+    break
+    }
+
+    switch(operation01){
+        case "Iene":
+           switch(operation02){
+            case "Real":
+        Proprio = 1
+        DolarAmericano = 0.0064
+        Euro = 0.0059
+        Real = 0.033
+        Peso = 5.69
+        
+        calculo = n1 * Real
+        resultado.innerHTML = calculo.toFixed(2)
+        break
+    }
+    break
+    }
+
+    switch(operation01){
+        case "Iene":
+           switch(operation02){
+            case "Peso":
+        Proprio = 1
+        DolarAmericano = 0.0064
+        Euro = 0.0059
+        Real = 0.033
+        Peso = 5.69
+        
+        calculo = n1 * Peso
+        resultado.innerHTML = calculo.toFixed(2)
+        break
+    }
+    break
+    }
+
+    switch(operation01){
+        case "Peso":
+           switch(operation02){
+            case "Peso":
+        Proprio = 1
+        DolarAmericano = 0.015
+        Euro = 0.013
+        Real = 0.075
+        Iene = 2.29
+        
+        calculo = n1 * Proprio
+        resultado.innerHTML = calculo.toFixed(2)
+        break
+    }
+    break
+    }
+
+    switch(operation01){
+        case "Peso":
+           switch(operation02){
+            case "Dolar Americano":
+        Proprio = 1
+        DolarAmericano = 0.015
+        Euro = 0.013
+        Real = 0.075
+        Iene = 2.29
+        
+        calculo = n1 * DolarAmericano
+        resultado.innerHTML = calculo.toFixed(2)
+        break
+    }
+    break
+    }
+
+    switch(operation01){
+        case "Peso":
+           switch(operation02){
+            case "Euro":
+        Proprio = 1
+        DolarAmericano = 0.015
+        Euro = 0.013
+        Real = 0.075
+        Iene = 2.29
+        
+        calculo = n1 * Euro
+        resultado.innerHTML = calculo.toFixed(2)
+        break
+    }
+    break
+    }
+
+    switch(operation01){
+        case "Peso":
+           switch(operation02){
+            case "Real":
+        Proprio = 1
+        DolarAmericano = 0.015
+        Euro = 0.013
+        Real = 0.075
+        Iene = 2.29
+        
+        calculo = n1 * Real
+        resultado.innerHTML = calculo.toFixed(2)
+        break
+    }
+    break
+    }
+
+    switch(operation01){
+        case "Peso":
+           switch(operation02){
+            case "Iene":
+        Proprio = 1
+        DolarAmericano = 0.015
+        Euro = 0.013
+        Real = 0.075
+        Iene = 2.29
+        
+        calculo = n1 * Iene
+        resultado.innerHTML = calculo.toFixed(2)
+        break
+    }
+    break
+    }
+}
